edd8b22e0fad0afd1187b48f0fdb4d3ed46883cc
Ian Solano <ian@madebymany.com>
2017-02-25 15:40:42 -0500

    refactoring client application structure

---
+++ client/containers/SignUp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUp = () => (
+  <div className='white'>signup form</div>
+);
+
+export default SignUp;
--- components/home/logged-out.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoggedOutHome = () => (
-  <div>
-    logged out wew
-  </div>
-);
-
-export default LoggedOutHome;
