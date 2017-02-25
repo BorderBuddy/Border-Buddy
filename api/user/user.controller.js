4e1fbded5a92b058ecc276d8d5eb92da4758e317
Emily H <emilyyho33@gmail.com>
2017-02-25 15:23:28 -0500

    sequelize added

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,12 +1,15 @@
 // import something from db
+import db from '../../database/db';
+import Traveler from '../../database/models/travelers'
 
 export function show(req, res) {
   const someDatabaseFxn = () => Promise.resolve();
-  someDatabaseFxn()
-    .then(() => {
-      res.status(200).json({ id: 123 });
-    }).catch((err) => {
-      console.log('err', err);
-      res.status(400).json(err);
-    });
+  // someDatabaseFxn()
+  //   .then(() => {
+  //     res.status(200).json({ id: 123 });
+  //   }).catch((err) => {
+  //     console.log('err', err);
+  //     res.status(400).json(err);
+  //   });
+  console.log("Hello")
 }
---
+++ database/index.js
@@ -0,0 +1,22 @@
+const chalk = require('chalk')
+const db = require('./db')
+
+// pull in models to database
+require('./models')
+
+// Sync the db, creating it if necessary
+const isTest = process.env.NODE_ENV === 'testing';
+const sync = (force = isTest) => {
+  return db.sync({ force })
+    .then(ok => console.log(chalk.green(`Synced ${db.config.database} database`)))
+    .catch(fail => {
+      console.log(chalk.yellow(`Creating ${db.config.database} database...`))
+      return new Promise((resolve, reject) =>
+        require('child_process').exec(`createdb "${db.config.database}"`, resolve)
+      ).then(() => sync(true))
+    })
+}
+
+db.didSync = sync();
+
+module.exports = db
