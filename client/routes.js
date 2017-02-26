9f0bfae7b35f585f6ec90628f6b62ec78daab002
Ian Solano <ian@madebymany.com>
2017-02-25 19:11:34 -0500

    adding signup for and api post

--- client/routes.js
+++ client/routes.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Route } from "react-router";
-import SignUp from "./containers/SignUp";
+import Homepage from "./containers/Homepage";
 import AllTravelers from "./containers/AllTravelers";
 import SingleTraveler from "./containers/SingleTraveler";
 import Login from './containers/Login';
 
 const getRoutes = () => (
 	<div>
-	  <Route path='/' component={SignUp} />
+	  <Route path='/' component={Homepage} />
 	  <Route path='/admin' component={AllTravelers} />
     <Route path='/admin/:id' component={SingleTraveler} />
     <Route path='/admin/login' component={Login} />
-	</div>
 );
 
 export default getRoutes;
