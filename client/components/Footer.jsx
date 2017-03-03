e4cab64d40f49b8a52b6218200d5131733a94d6f
Dillon Powers <dillon.c.powers@gmail.com>
2017-03-03 16:54:19 -0500

    added github link

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -6,9 +6,14 @@ export default () => (
 			<h4 className="m0 center footer-txt">
 				Built with love by BorderBuddy &copy; 2017
 			</h4>
-			<a className="m0 center" href="mailto:admin@borderbuddy.us" target="_blank">
+			<a className="mx-auto block" href="mailto:admin@borderbuddy.us" target="_blank">
 					admin@borderbuddy.us
 			</a>
+			<a className="mx-auto block" href="https://github.com/EmilyDev/Border-Buddy" target="_blank">
+					Github
+			</a>
+
 		</footer>
 
 );
+
