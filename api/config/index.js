9a5ee9285bee11f84fdc72504aeaae3d8cc45b96
Dillon Powers <dillon.c.powers@gmail.com>
2017-03-02 21:35:15 -0500

    texting ok will update user status in db

--- api/config/index.js
+++ api/config/index.js
@@ -13,10 +13,9 @@ export var config = {
 	  password: 'root'
   },
   twilio: {
-    adminPhone: process.env.TWILIO_PHONE_NUM || '+15005550006', // test num only https://www.twilio.com/docs/api/rest/test-credentials
+    adminPhone: process.env.TWILIO_PHONE_NUM || '+15005550006', // limited functionality https://www.twilio.com/docs/api/rest/test-credentials
     messagingSid: process.env.TWILIO_MESSAGING_SID || 'MGba927c2be4ce4dceff68aa24b4e85a32',
     accountSid: process.env.TWILIO_ACCOUNT_SID || 'ACdf9040cbb781f0eb315a6ef0bd4def99', // test version hard-coded
     authToken: process.env.TWILIO_AUTH_TOKEN || '246e9034039a5bdfeb5e8fd2027f42c9' // test version hard-coded
   }
 };
-
--- api/twilio/twilio.controller.js
+++ api/twilio/twilio.controller.js
@@ -2,20 +2,16 @@ import { config } from '../config';
 import Traveler from '../../database/models/travelers';
 import _ from 'lodash';
 const Twilio = require('twilio')(config.twilio.accountSid, config.twilio.authToken);
-const chalk = require('chalk')
 
 // admin hits this api to send text to users
 export const sendText = (req, res, next) => {
 	if (_.isEmpty(req.body)) {
-		let err = new Error('No message body');
-		err.status = 404;
-		next(err);
+		return res.status(404).end();
 	}
 
 	Twilio.sendMessage({
 	    to: req.body.to,
 	    from: config.twilio.adminPhone,
-	    // messagingServiceSid: config.twilio.messagingSid, // not working in dev mode so removed
 	    body: req.body.message,
 	}, (err, result) => {
 		if (err) next(err);
@@ -24,49 +20,55 @@ export const sendText = (req, res, next) => {
 
 };
 
+// webhook from twilio will hit this route
 export const respondToText = (req, res, next) => {
-	console.log('JUST HIT THIS ROUTE', req)
-	console.log(chalk.blue('REQ BODY IS', req.body))
-	var response = req.body.Body;
-	var phoneStrTo = req.body.To.substring(req.body.To.length - 11);
-	var phoneStrFrom = req.body.From.substring(req.body.From.length - 10);
+	const msgBody = req.body.Body;
+	const travelerPhone = req.body.From.slice(2, req.body.From.length); // remove +1
 
-	if (response.toLowerCase() === 'ok') {
-		return Traveler.find({
-		    where: {
-					phone: phoneStrTo
-		    }
-		})
-		.then(traveler => {
-		    if (!traveler) {
-					return res.status(404).end();
-		    }
-		    traveler.status = 'cleared';
-		    traveler.save()
-				.then(function(traveler) {
-			    return res.json(traveler);
-		    });
-
-		    return res.status(200).end();
-		})
-		.catch(next);
-	} else {
-		// TODO: Scheduler for sending a text again to user prompting status if ok
+	if (req.body.AccountSid !== config.twilio.accountSid) {
+		return res.status(403).end();
 	}
 
-	return res.status(200).json({});
-}
+	if (msgBody.toLowerCase() !== 'ok') {
+		return res.status(400).end();
+	}
+
+	Traveler.findOne({
+    where: {
+			phone: travelerPhone
+    }
+	})
+	.then(traveler => {
+    if (!traveler) {
+			return res.status(404).end();
+    }
+    traveler.status = 'cleared';
+    return traveler.save();
+	})
+	.then(traveler => {
+		const xml = `
+		<?xml version="1.0" encoding="UTF-8"?>
+		<Response>
+		    <Message>Welcome home, ${traveler.name}! You have been marked as cleared. Thanks for using BorderBuddy.</Message>
+		</Response>`;
+		res.status(200).send(xml);
+	})
+	.catch(next);
+};
 
 
 /*------------ HELPER FUNCTION FOR CRON JOB ONLY -------------*/
 
 export const sendVerification = (body, user) => {
-	Twilio.sendMessage({
-
-		to: user.phone,
-		from: config.twilio.adminPhone,
-		body
-
+	return new Promise((resolve, reject) => {
+		Twilio.sendMessage({
+			to: user.phone,
+			from: config.twilio.adminPhone,
+			body
+		}, (err, result) => {
+			if (err) reject(err);
+			else resolve(result);
+		});
 	});
 };
 
