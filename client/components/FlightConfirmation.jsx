2a42b8550c3074045211239141c73f157f477a88
Dillon Powers <dillon.c.powers@gmail.com>
2017-03-06 13:11:41 -0500

    fixed bug in flight confirmation

--- client/components/FlightConfirmation.jsx
+++ client/components/FlightConfirmation.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
 
 export default ({ flight }) => {
-	const departureDate = new Date(flight.scheduledFlights[0].departureTime);
-	const arrivalDate = new Date(flight.scheduledFlights[0].arrivalTime);
+	const departureDate = flight.scheduledFlights[0].departureTime.split('T');
+	const arrivalDate = flight.scheduledFlights[0].arrivalTime.split('T');
+	const { arrivalAirportFsCode, departureAirportFsCode, carrierFsCode, flightNumber } = flight.scheduledFlights[0];
+	const { airlines } = flight.appendix;
 
-	let depMins = departureDate.getMinutes().toString();
-	let arrMins = arrivalDate.getMinutes().toString();
+	let arrivalCityName, arrivalAirport, departureCityName, departureAirport, airlineName;
 
-	if (depMins.length !== 2) depMins = `0${depMins}`;
-	if (arrMins.length !== 2) arrMins = `0${arrMins}`;
+	const airport1 = flight.appendix.airports[0];
+	const airport2 = flight.appendix.airports[1];
+
+	if (airport1.iata === arrivalAirportFsCode) {
+		arrivalCityName = airport1.city;
+		arrivalAirport = airport1.name;
+		departureCityName = airport2.city;
+		departureAirport = airport2.name;
+	} else {
+		arrivalCityName = airport2.city;
+		arrivalAirport = airport2.name;
+		departureCityName = airport1.city;
+		departureAirport = airport1.name;
+	}
+
+	for (let i = 0; i < airlines.length; i++) {
+		if (airlines[i].iata === carrierFsCode) {
+			airlineName = airlines[i].name;
+		}
+	}
+
 
 	return (
 		<div id="flight-confirmation-container" className="clearfix">
 		  <div className="col-12">
+				<h3>{airlineName} {flightNumber} {departureAirportFsCode}-{arrivalAirportFsCode}</h3>
+		  </div>
+		  <div className="col-12">
 				<h3 className="center m1">Departing</h3>
-				<h4 className="m1">From:  {flight.appendix.airports[0].cityCode} - {flight.appendix.airports[0].name}</h4>
-				<h4 className="m1">Date:  {departureDate.getYear() + 1900} / {departureDate.getMonth() + 1} / {departureDate.getDate()}</h4>
-				<h4 className="m1">Time:  {departureDate.getHours()}:{depMins}</h4>
+				<h4 className="m1">From:  {departureCityName} - {departureAirport}</h4>
+				<h4 className="m1">Date:  {departureDate[0]}</h4>
+				<h4 className="m1">Time:  {departureDate[1].slice(0, 5)}</h4>
 		  </div>
 		  <div className="col-12">
 				<h3 className="center m1">Arriving</h3>
-				<h4 className="m1">To:  {flight.appendix.airports[1].cityCode} - {flight.appendix.airports[1].name}</h4>
-				<h4 className="m1">Date:  {arrivalDate.getYear() + 1900} / {arrivalDate.getMonth() + 1} / {arrivalDate.getDate()}</h4>
-				<h4 className="m1">Time:  {arrivalDate.getHours()}:{arrMins}</h4>
+				<h4 className="m1">To:  {arrivalCityName} - {arrivalAirport}</h4>
+				<h4 className="m1">Date:  {arrivalDate[0]}</h4>
+				<h4 className="m1">Time:  {arrivalDate[1].slice(0, 5)}</h4>
 		  </div>
 
 		</div>
