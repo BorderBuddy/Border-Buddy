5de204f97c0dd107defe713ef068e22f04254509
Dillon Powers <dillon.c.powers@gmail.com>
2017-03-06 10:30:14 -0500

    styling about and why borderbuddy

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -8,23 +8,29 @@ export default () => (
 		<div className="col-12">
 			<p className="center">We started at Hack the Ban</p>
 			<a href="vocativ.com/405756/coders-hack-trump-immigration-ban/" target="_blank" rel="noopener noreferrer">
-				<img src="hacktheban.png" />
+				<img className="hacktheban-link" src="/images/hacktheban.png" />
 			</a>
 
 			<p className="center">Now we're testing our app with real users</p>
 			<p className="center">If you'd like to contribute, check out our Github</p>
 			
-			<a href="http://www.cunyclear.org/" target="_blank" rel="noopener noreferrer">
-				<img src="CLEAR.png" />
-			</a>
+			<div className="col-6 inline-block">
+				<a href="http://www.cunyclear.org/" target="_blank" rel="noopener noreferrer">
+					<img className="partner-link" src="/images/CLEAR.png" />
+				</a>
+			</div>
 
-			<a href="https://mpowerchange.org/" target="_blank" rel="noopener noreferrer">
-				<img src="mpower.png" />
-			</a>
-			
-			<a href="https://github.com/EmilyDev/Border-Buddy" target="_blank" rel="noopener noreferrer">
-				<img src="github.png" />
-			</a>
+			<div className="col-6 inline-block">
+				<a href="https://mpowerchange.org/" target="_blank" rel="noopener noreferrer">
+					<img className="partner-link" src="/images/mpower.png" />
+				</a>
+			</div>
+
+			<div className="col-12">
+				<a href="https://github.com/EmilyDev/Border-Buddy" target="_blank" rel="noopener noreferrer">
+					<img className="github-link" src="/images/github.png" />
+				</a>
+			</div>
 
 		</div>
 	</div>
