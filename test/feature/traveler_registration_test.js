f58778eb25bfe00c23404fb00e550c63c1ff75f4
Dillon Powers, dpowers, Neha Kumari and Oliver Switzer <pair+dillon+nkumari+oswitzer@pivotal.io>
2017-03-15 12:18:29 -0400

    Add a run script for running tests

--- test/feature/traveler_registration_test.js
+++ test/feature/traveler_registration_test.js
@@ -17,7 +17,7 @@ describe('BorderBuddy', () => {
     })
       .viewport(1280, 1024);
 
-    return nightmare.goto('http://localhost:8080')
+    return nightmare.goto('http://localhost:3000')
       .wait('#homepage')
       .clickOnElementContaining('a', 'Register')
       .type('input[name="name"]', 'Jane Citizen')
@@ -39,7 +39,7 @@ describe('BorderBuddy', () => {
       .wait('#submit-flight-confirmation')
       .mouseDownUpOnElementContaining('#submit-flight-confirmation', 'Submit')
       .wait('#success-container')
-      .goto('http://localhost:8080/admin')
+      .goto('http://localhost:3000/admin')
       .type('input[name="email"]', 'admin@borderbuddy.us')
       .type('input[name="password"]', '1234')
       .clickOnElementContaining('button', 'Login')
