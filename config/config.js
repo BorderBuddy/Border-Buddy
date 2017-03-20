82d7b8c4040114b87e70205141a20cda5115beef
Oliver Switzer and Sarah Michaelson <pair+oswitzer+smichaelson@pivotal.io>
2017-03-20 13:58:45 -0400

    Be explicit about our username and db name

--- config/config.js
+++ config/config.js
@@ -1,10 +1,14 @@
 module.exports = {
   'development' : {
     url: 'postgres://postgres:root@localhost:5432/BorderBuddy',
+    username: 'postgres',
+    database: 'BorderBuddy',
     dialect: 'postgres'
   },
   'test' : {
     url: 'postgres://postgres:root@localhost:5432/BorderBuddy_test',
+    username: 'postgres',
+    database: 'BorderBuddy_test',
     dialect: 'postgres'
   },
   'production': {
