6e9c8ccf6f7447fff2bb83f44ab53a436230d7d6
Emily Ho and Mike Kent <pair+eyho+mkent@pivotal.io>
2017-03-23 16:18:04 -0400

    Adds throttling to admin login page
    NOTES:
    - After 3 attempts, user will have to wait 2 seconds before trying again
    - Introduces dependencies on express-brute, express-brute-sequelize and sqlite3
    - Creates a new table `bruteStore` in database for tracking info

--- api/auth/auth.route.js
+++ api/auth/auth.route.js
@@ -1,6 +1,9 @@
 import * as passportConfig from './passport';
 import {login, isAuthenticated, logout} from './auth.controller';
 import {User} from '../../database/models/user';
+import sequelize from '../../database/db';
+import ExpressBrute from 'express-brute';
+import SequelizeStore from 'express-brute-sequelize';
 
 const base = '/api/auth';
 
@@ -8,6 +11,19 @@ passportConfig.setup(User);
 
 export default (app) => {
   app.get(base + '/checkToken', isAuthenticated);
-  app.post(base + '/local', login);
   app.post(base + '/logout', logout);
+
+  new SequelizeStore(sequelize, 'bruteStore', {}, (store) => {
+    const bruteforce = new ExpressBrute(store,
+      {
+        freeRetries: 3,
+        minWait: 2*1000,
+        attachResetToRequest: false,
+        refreshTimeoutOnRequest: true
+      });
+
+      app.post(base + '/local',
+        bruteforce.prevent,
+        login);
+  });
 };
