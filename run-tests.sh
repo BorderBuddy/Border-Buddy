5199425d630f774dbd4200d51dbac0fae06c4f24
Oliver Switzer and Sarah Michaelson <pair+oswitzer+smichaelson@pivotal.io>
2017-03-24 14:29:40 -0400

    Sleep for 3 seconds instead of 10 in run-tests.sh--10 seconds was overkill for starting the webpack and api servers

--- run-tests.sh
+++ run-tests.sh
@@ -8,12 +8,12 @@ npm run seed
 if [ "$LOCAL" == "1" ]; then
     export TEST_PORT=8080
     npm run dev&
-    sleep 10
+    sleep 3
 else
     export TEST_PORT=3000
     npm run build
 fi
 
 npm run start&
-sleep 10
+sleep 3
 ./node_modules/mocha/bin/mocha
