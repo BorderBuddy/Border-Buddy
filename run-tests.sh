67984c40970b4d80980020c1f1b67f9455dc94f7
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2018-03-27 21:19:01 -0400

    changed run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -7,14 +7,15 @@ npm run seed
 
 if [ "$LOCAL" == "1" ]; then
     export TEST_PORT=8080
-    # npm run dev&
+    npm run dev&
     sleep 3
 else
     export TEST_PORT=3000
-    # npm run build
+    npm run build
     sleep 5
 fi
 
 npm run start&
 sleep 3
 ./node_modules/mocha/bin/mocha
+jest
