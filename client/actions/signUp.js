f9c3c8880c3db27394d914adc4489760a374d9ac
Aaron Arntz <aarntz@gmail.com>
2020-05-20 17:24:40 -0400

    add basic ts skeleton

--- client/actions/signUp.js
+++ client/actions/signUp.js
@@ -1,32 +1,31 @@
 import {
   SET_SIGNUP_TRAVELER,
   CLEAR_SIGNUP_TRAVELER
-} from '../constants';
+} from '../constants'
 
-import axios from 'axios';
-import { browserHistory } from 'react-router';
+import axios from 'axios'
+import { browserHistory } from 'react-router'
 
 const setSignupTraveler = traveler => ({
-	type: SET_SIGNUP_TRAVELER,
-	traveler
-});
+  type: SET_SIGNUP_TRAVELER,
+  traveler
+})
 
 export const signUpTraveler = (traveler, isAdmin) => {
-	if (traveler.countryCode[0] === '+') traveler.countryCode = traveler.countryCode.slice(1);
-	// NOTE: we really should make the code an enum
+  if (traveler.countryCode[0] === '+') traveler.countryCode = traveler.countryCode.slice(1)
+  // NOTE: we really should make the code an enum
 
   return dispatch => {
-		axios.post('/api/traveler/', traveler)
-		.then(res => {
-			dispatch(setSignupTraveler(res.data));
-			if(!isAdmin) browserHistory.push('/success');
-			else browserHistory.push('/admin/travelers')
-		})
-		.catch(console.error);
-  };
-};
+    axios.post('/api/traveler/', traveler)
+      .then(res => {
+        dispatch(setSignupTraveler(res.data))
+        if (!isAdmin) browserHistory.push('/success')
+        else browserHistory.push('/admin/travelers')
+      })
+      .catch(console.error)
+  }
+}
 
 export const clearSignUpTraveler = () => ({
-	type: CLEAR_SIGNUP_TRAVELER
-});
-
+  type: CLEAR_SIGNUP_TRAVELER
+})
--- client/containers/Root.js
+++ client/containers/Root.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router';
-import getRoutes from '../routes';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { Router } from 'react-router'
+import getRoutes from '../routes'
 
 // needed for Material-UI and onTouchTap events
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 // custom Mui Theme
-import muiTheme from '../utils/muiTheme';
+import muiTheme from '../utils/muiTheme'
+injectTapEventPlugin()
 
 const Root = ({ store, history }) => (
   <Provider store={store}>
-		<MuiThemeProvider muiTheme={muiTheme}>
+    <MuiThemeProvider muiTheme={muiTheme}>
 	    <Router history={history}>
 	      {getRoutes(store)}
 	    </Router>
     </MuiThemeProvider>
   </Provider>
-);
+)
 
-export default Root;
+export default Root
