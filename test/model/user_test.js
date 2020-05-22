67210cec0a25c2d85db63e0bb055a0e87952e88e
Aaron Arntz <aarntz@gmail.com>
2020-05-22 16:33:40 -0400

    got one test running

--- test/model/user_test.js
+++ test/model/user_test.js
@@ -1,41 +1,41 @@
-import { User } from '../../database/models/user';
-import { expect } from 'chai';
+import { User } from '../../database/models/user'
+import { expect } from 'chai'
 
 describe('Model: User', () => {
   describe('validations', () => {
     describe('password length must be at least 8 characters', () => {
       it('throws an error when less than 8', () => {
-        const user = User.build({ password: 'short' });
+        const user = User.build({ password: 'short' })
 
         return user.validate().then((result) => {
-          let passwordErrors = getErrorsForField(result, 'password');
-          expect(passwordErrors).to.contain('Must be at least 8 characters long');
-        });
-      });
+          const passwordErrors = getErrorsForField(result, 'password')
+          expect(passwordErrors).to.contain('Must be at least 8 characters long')
+        })
+      })
 
       it('does not throw an error when equal to 8', () => {
-        const user = User.build({ password: 'exactly8' });
+        const user = User.build({ password: 'exactly8' })
 
         return user.validate().then((result) => {
-          let passwordErrors = getErrorsForField(result, 'password');
-          expect(passwordErrors.length).to.equal(0);
-        });
-      });
+          const passwordErrors = getErrorsForField(result, 'password')
+          expect(passwordErrors.length).to.equal(0)
+        })
+      })
 
       it('does not throw an error when more than 8', () => {
-        const user = User.build({ password: 'long-enough' });
+        const user = User.build({ password: 'long-enough' })
 
         return user.validate().then((result) => {
-          let passwordErrors = getErrorsForField(result, 'password');
-          expect(passwordErrors.length).to.equal(0);
-        });
-      });
-    });
-  });
-});
+          const passwordErrors = getErrorsForField(result, 'password')
+          expect(passwordErrors.length).to.equal(0)
+        })
+      })
+    })
+  })
+})
 
-function getErrorsForField(validationResult, field) {
+function getErrorsForField (validationResult, field) {
   return validationResult.errors
     .filter((error) => error.path == field)
-    .map((fieldError) => fieldError.message);
+    .map((fieldError) => fieldError.message)
 }
