619cb415511d5ff8108b12d4be9b94cf2e1d04fa
Aaron Arntz <aarntz@gmail.com>
2020-05-26 17:29:28 -0400

    get a postman jwt

--- api/user/user.route.js
+++ api/user/user.route.js
@@ -6,17 +6,17 @@ import {
   me,
   changePassword,
   update
-} from './user.controller';
-import {protectedEndpoint} from '../auth/auth.service';
+} from './user.controller'
+import { protectedEndpoint } from '../auth/auth.service'
 
-const base = '/api/user';
+const base = '/api/user'
 
 export default (app) => {
-  app.get(base + '/', protectedEndpoint(index));
-  app.get(base + '/me', protectedEndpoint(me));
-  app.get(base + '/:id', protectedEndpoint(show));
-  app.post(base + '/', protectedEndpoint(create));
-  app.put(base + '/:id', protectedEndpoint(update));
-  app.put(base + '/:id/password', protectedEndpoint(changePassword));
-  app.delete(base + '/:id', protectedEndpoint(destroy));
-};
+  app.get(base + '/', protectedEndpoint(index))
+  app.get(base + '/me', protectedEndpoint(me))
+  app.get(base + '/:id', protectedEndpoint(show))
+  app.post(base + '/', protectedEndpoint(create))
+  app.put(base + '/:id', protectedEndpoint(update))
+  app.put(base + '/:id/password', protectedEndpoint(changePassword))
+  app.delete(base + '/:id', protectedEndpoint(destroy))
+}
