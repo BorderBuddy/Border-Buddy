0c8fb356a6f19b01534215bbaac6f374b8dd6ee2
Aaron Arntz <aarntz@gmail.com>
2020-06-02 17:43:32 -0400

    move stuff around

--- packages/client/scaffold.webpack.dev.conf.js
+++ packages/client/scaffold.webpack.dev.conf.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpack = require('webpack')
 const autoprefixer = require('autoprefixer')
-const CopyPlugin = require('copy-webpack-plugin')
+// const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const paths = {
   distFolder: path.resolve(__dirname, 'dist_dev'),
@@ -37,9 +38,56 @@ module.exports = {
 
   module: {
     rules: [
-      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-      { test: /\.(t|j)sx?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
-      { enforce: 'pre', test: /\.js$/, exclude: /node_modules/, loader: 'source-map-loader' }
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(t|j)sx?$/,
+        use: { loader: 'ts-loader' },
+        exclude: /node_modules/
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'source-map-loader'
+      },
+      {
+        include: /\.json$/,
+        loader: 'json'
+      }
+      // {
+      //   test: /\.ts(x?)$/,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     {
+      //       loader: 'ts-loader'
+      //     }
+      //   ]
+      // },
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   exclude: /(node_modules)/,
+      //   loader: 'babel-loader',
+      //   query: {
+      //     presets: ['es2015', 'react', 'stage-2'],
+      //     plugins: ['transform-object-rest-spread']
+      //   }
+      // },
+      // {
+      //   test: /\.js?$/,
+      //   exclude: [
+      //     path.resolve(__dirname, 'node_modules'),
+      //     path.resolve(__dirname, 'dist')
+      //   ]
+      // },
+      // {
+      //   test: /\.scss$/,
+      //   exclude: /(node_modules)/,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      // },
+
       // // Lint TypeScript
       // {
       //   test: /\.tsx?$/,
@@ -110,23 +158,31 @@ module.exports = {
       filename: 'index.html'
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new CopyPlugin([
-      {
-        from: path.resolve(paths.assetsFolder, 'favicon.ico'),
-        to: path.resolve(paths.distFolder, 'favicon.ico')
-      },
-      {
-        from: path.resolve(paths.assetsFolder, 'manifest.json'),
-        to: path.resolve(paths.distFolder, 'manifest.json')
-      }
-    ])
+    new webpack.NamedModulesPlugin(),
+    new MiniCssExtractPlugin('stylesheets/fund.compiled.css'),
+    new webpack.LoaderOptionsPlugin({ options: {} }),
+    new webpack.DefinePlugin({
+      DEV: true
+    })
+    // new CopyPlugin([
+    //   {
+    //     from: path.resolve(paths.assetsFolder, 'favicon.ico'),
+    //     to: path.resolve(paths.distFolder, 'favicon.ico')
+    //   },
+    //   {
+    //     from: path.resolve(paths.assetsFolder, 'manifest.json'),
+    //     to: path.resolve(paths.distFolder, 'manifest.json')
+    //   }
+    // ])
   ],
   devServer: {
     contentBase: paths.assetsFolder,
-    index: 'index.html',
+    publicPath: '/',
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
     hot: true,
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': 'http://localhost:3000'
     }
   }
 }
