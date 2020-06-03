ab807dffd10a850d57eb1bdf19aa5cdcba2dde89
Aaron Arntz <aarntz@gmail.com>
2020-06-03 11:41:16 -0400

    webpack builds with TS

---
+++ packages/client/src/old.index.tsx
@@ -0,0 +1,14 @@
+import 'isomorphic-fetch'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './containers/Root'
+import configureStore from './store'
+const { store, history } = configureStore()
+export { store }
+
+require('./stylesheets/index.scss')
+
+ReactDOM.render(
+  <Root store={store} history={history} />,
+  document.getElementById('root')
+)
