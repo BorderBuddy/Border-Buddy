0bd424251ceffb1ff2cdea9bcf3af1ac1e08072b
Aaron Arntz <aarntz@gmail.com>
2020-06-04 17:31:58 -0400

    add homepage build

--- packages/client/src/containers/Root.jsx
+++ packages/client/src/containers/Root.jsx
@@ -2,23 +2,16 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { getRoutes } from '../routes'
-
-// needed for Material-UI and onTouchTap events
-// import { MuiThemeProvider } from '@material-ui/styles'
-// import injectTapEventPlugin from 'react-tap-event-plugin'
-
-// custom Mui Theme
-// import muiTheme from '../utils/muiTheme'
-// injectTapEventPlugin()
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
+import muiTheme from '../utils/muiTheme'
 
 // eslint-disable-next-line react/prop-types
 export const Root = ({ store }) => (
   <Provider store={store}>
-    {/* <MuiThemeProvider muiTheme={muiTheme}> */}
-    {/* <h1>Welcome to the App!</h1> */}
-    <BrowserRouter>
-      {getRoutes()}
-    </BrowserRouter>
-    {/* </MuiThemeProvider> */}
+    <MuiThemeProvider theme={muiTheme}>
+      <BrowserRouter>
+        {getRoutes()}
+      </BrowserRouter>
+    </MuiThemeProvider>
   </Provider>
 )
--- packages/client/src/containers/SignUpContainer.jsx
+++ packages/client/src/containers/SignUpContainer.jsx
@@ -1,52 +1,52 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { reduxForm } from 'redux-form';
-import { Dialog, FlatButton, RaisedButton } from 'material-ui';
-import Form from '../components/FormContainer';
-import FlightConfirmation from '../components/FlightConfirmation';
-import { signUpTraveler } from '../actions/signUp';
-import { checkFlight } from '../actions/flight';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { reduxForm } from 'redux-form'
+import { Dialog, FlatButton, RaisedButton } from 'material-ui'
+import Form from '../components/FormContainer'
+import FlightConfirmation from '../components/FlightConfirmation'
+import { signUpTraveler } from '../actions/signUp'
+import { checkFlight } from '../actions/flight'
 
 class SignUpContainer extends Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
-      open: false,
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.confirmSubmit = this.confirmSubmit.bind(this);
+      open: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.confirmSubmit = this.confirmSubmit.bind(this)
   }
 
-  handleClose() {
-    this.setState({open: false});
+  handleClose () {
+    this.setState({ open: false })
   }
 
-  confirmSubmit() {
-    const { signUpTraveler, flight } = this.props;
-    const { values } = this.props.form.travelerForm;
-    values.countryCode = values.countryCode.split('-')[1].slice(2);
-    const travelerInfo = Object.assign({}, values, { arrivalTime: flight.arrivalTimeUtc });
-    signUpTraveler(travelerInfo);
-    this.handleClose();
+  confirmSubmit () {
+    const { signUpTraveler, flight } = this.props
+    const { values } = this.props.form.travelerForm
+    values.countryCode = values.countryCode.split('-')[1].slice(2)
+    const travelerInfo = Object.assign({}, values, { arrivalTime: flight.arrivalTimeUtc })
+    signUpTraveler(travelerInfo)
+    this.handleClose()
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const { flightNum, airlineCode, arrivalTime } = this.props.form.travelerForm.values;
-    const day = arrivalTime.getDate();
-    const year = arrivalTime.getYear() + 1900;
-    const month = arrivalTime.getMonth() + 1;
+  handleSubmit (e) {
+    e.preventDefault()
+    const { flightNum, airlineCode, arrivalTime } = this.props.form.travelerForm.values
+    const day = arrivalTime.getDate()
+    const year = arrivalTime.getYear() + 1900
+    const month = arrivalTime.getMonth() + 1
     this.props.checkFlight(airlineCode, flightNum, year, month, day)
-    .then(() => {
-      this.setState({ open: true });
-    })
-    .catch(() => {
-      this.setState({ open: false });
-    });
+      .then(() => {
+        this.setState({ open: true })
+      })
+      .catch(() => {
+        this.setState({ open: false })
+      })
   }
 
-  render() {
+  render () {
     const confirmActions = [
       <FlatButton
         label="Cancel"
@@ -59,7 +59,7 @@ class SignUpContainer extends Component {
         primary={true}
         onTouchTap={this.confirmSubmit}
       />
-    ];
+    ]
 
     const cancelActions = [
       <FlatButton
@@ -67,7 +67,7 @@ class SignUpContainer extends Component {
         primary={true}
         onTouchTap={this.handleClose}
       />
-    ];
+    ]
 
     return (
       <div>
@@ -81,22 +81,20 @@ class SignUpContainer extends Component {
           <FlightConfirmation flight={this.props.flight} />
         </Dialog>
       </div>
-    );
+    )
   }
 }
 
-/*---------------------------REDUX CONTAINER---------------------------*/
+/* ---------------------------REDUX CONTAINER--------------------------- */
 
-
-const mapStateToProps = ({form, flight}) => ({form, flight});
+const mapStateToProps = ({ form, flight }) => ({ form, flight })
 
 const mapDispatchToProps = dispatch => ({
   signUpTraveler: traveler => dispatch(signUpTraveler(traveler)),
   checkFlight: (code, flightNum, year, month, day) => dispatch(checkFlight(code, flightNum, year, month, day))
-});
+})
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(SignUpContainer);
-
+)(SignUpContainer)
