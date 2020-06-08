47cc5956ebae459316f8ee41a8666644a7f55648
Aaron Arntz <aarntz@gmail.com>
2020-06-08 17:12:13 -0400

    install test dependencies, fix paths

--- test/feature/nightmare_helpers.js
+++ test/feature/nightmare_helpers.js
@@ -1,70 +1,68 @@
-import Nightmare from 'nightmare';
+import Nightmare from 'nightmare'
 
 Nightmare.action('clickOnElementContaining', function (selector, text, done) {
   this.evaluate_now(function (selector, text) {
-    document.activeElement.blur();
+    document.activeElement.blur()
 
-    const found = [];
-    const downcaseText = text.toLowerCase();
+    const found = []
+    const downcaseText = text.toLowerCase()
     document.querySelectorAll(selector).forEach((element) => {
       if (element.innerText.toLowerCase().indexOf(downcaseText) >= 0) {
-        found.push(element);
+        found.push(element)
       }
-    });
+    })
 
     if (found.length < 1) {
-      throw new Error(`Unable to find element by selector ${selector} containing text: ${text}`);
+      throw new Error(`Unable to find element by selector ${selector} containing text: ${text}`)
     }
 
-    const event = document.createEvent('MouseEvent');
-    event.initEvent('click', true, true);
-    found[0].dispatchEvent(event);
-  }, done, selector, text);
-});
+    const event = document.createEvent('MouseEvent')
+    event.initEvent('click', true, true)
+    found[0].dispatchEvent(event)
+  }, done, selector, text)
+})
 
 Nightmare.action('mouseDownUpOnElement', function (selector, done) {
-  this.evaluate_now(function (selector,) {
-    document.activeElement.blur();
+  this.evaluate_now(function (selector) {
+    document.activeElement.blur()
 
-    const found = document.querySelector(selector);
+    const found = document.querySelector(selector)
     if (!found) {
-      throw new Error(`Unable to find element by selector ${selector}`);
+      throw new Error(`Unable to find element by selector ${selector}`)
     }
 
-    const events = ['mousedown', 'mouseup'];
+    const events = ['mousedown', 'mouseup']
 
     events.forEach(function (type) {
-      const event = document.createEvent('MouseEvent');
-      event.initEvent(type, true, true);
-      found.dispatchEvent(event);
-    });
-
-  }, done, selector,);
-});
+      const event = document.createEvent('MouseEvent')
+      event.initEvent(type, true, true)
+      found.dispatchEvent(event)
+    })
+  }, done, selector)
+})
 
 Nightmare.action('mouseDownUpOnElementContaining', function (selector, text, done) {
   this.evaluate_now(function (selector, text) {
-    document.activeElement.blur();
+    document.activeElement.blur()
 
-    const found = [];
-    const downcaseText = text.toLowerCase();
+    const found = []
+    const downcaseText = text.toLowerCase()
     document.querySelectorAll(selector).forEach((element) => {
       if (element.innerText.toLowerCase().indexOf(downcaseText) >= 0) {
-        found.push(element);
+        found.push(element)
       }
-    });
+    })
 
     if (found.length < 1) {
-      throw new Error(`Unable to find element by selector ${selector} containing text: ${text}`);
+      throw new Error(`Unable to find element by selector ${selector} containing text: ${text}`)
     }
 
-    const events = ['mousedown', 'mouseup'];
+    const events = ['mousedown', 'mouseup']
 
     events.forEach(function (type) {
-      const event = document.createEvent('MouseEvent');
-      event.initEvent(type, true, true);
-      found[0].dispatchEvent(event);
-    });
-
-  }, done, selector, text);
-});
+      const event = document.createEvent('MouseEvent')
+      event.initEvent(type, true, true)
+      found[0].dispatchEvent(event)
+    })
+  }, done, selector, text)
+})
