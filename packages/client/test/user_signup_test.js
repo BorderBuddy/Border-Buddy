f23ceac3155b62c9b9a283fb475aec4bf08f0640
Aaron Arntz <aarntz@gmail.com>
2020-06-08 17:12:13 -0400

    install test dependencies, fix paths

--- packages/client/test/user_signup_test.js
+++ packages/client/test/user_signup_test.js
@@ -1,31 +1,30 @@
-import './../unit_helpers';
-import {signup} from '../../client/actions/auth';
-import axios from 'axios';
+import './unit_helpers'
+import { signup } from '../src/actions/auth'
+import axios from 'axios'
 
 describe('User', () => {
-  let sandbox;
+  let sandbox
   beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-  });
+    sandbox = sinon.sandbox.create()
+  })
 
   afterEach(() => {
-    sandbox.restore();
-  });
+    sandbox.restore()
+  })
 
   describe('Creating new admin user', () => {
     it('sends a post request to create user api with accessToken', () => {
-      const window = {localStorage: {accessToken: 'accessToken'}};
+      const window = { localStorage: { accessToken: 'accessToken' } }
 
       sandbox.stub(axios, 'post', (url, user, headers) => {
-        expect(url).to.equal('/api/user');
-        expect(user.name).to.equal('Jane Austen');
-        expect(headers.headers['Authorization']).to.equal('accessToken');
+        expect(url).to.equal('/api/user')
+        expect(user.name).to.equal('Jane Austen')
+        expect(headers.headers.Authorization).to.equal('accessToken')
         return new Promise((resolve, reject) => {
-        });
-      });
+        })
+      })
 
-      signup({name: 'Jane Austen'}, window)();
-    });
-  });
-});
-
+      signup({ name: 'Jane Austen' }, window)()
+    })
+  })
+})
