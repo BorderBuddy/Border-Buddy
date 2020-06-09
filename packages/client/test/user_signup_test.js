ff1cff4031af2df00909f1cc12d6bb09d296bef3
Aaron Arntz <aarntz@gmail.com>
2020-06-09 16:23:13 -0400

    migrate sinon

--- packages/client/test/user_signup_test.js
+++ packages/client/test/user_signup_test.js
@@ -3,20 +3,17 @@ import { signup } from '../src/actions/auth'
 import axios from 'axios'
 
 describe('User', () => {
-  let sandbox
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create()
-  })
+  beforeEach(() => {})
 
   afterEach(() => {
-    sandbox.restore()
+    sinon.restore()
   })
 
   describe('Creating new admin user', () => {
     it('sends a post request to create user api with accessToken', () => {
       const window = { localStorage: { accessToken: 'accessToken' } }
 
-      sandbox.stub(axios, 'post', (url, user, headers) => {
+      sinon.stub(axios, 'post').callsFake((url, user, headers) => {
         expect(url).to.equal('/api/user')
         expect(user.name).to.equal('Jane Austen')
         expect(headers.headers.Authorization).to.equal('accessToken')
