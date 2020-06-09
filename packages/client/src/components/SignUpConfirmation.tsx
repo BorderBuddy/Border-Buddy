3ad12585c6bb5b5200cc480184dedca64ea36f33
Aaron Arntz <aarntz@gmail.com>
2020-06-09 16:23:13 -0400

    migrate sinon

--- packages/client/src/components/SignUpConfirmation.tsx
+++ packages/client/src/components/SignUpConfirmation.tsx
@@ -1,29 +1,29 @@
 import React from 'react'
-import { Dialog, Button } from '@material-ui/core'
+import { Dialog, Button, DialogActions } from '@material-ui/core'
 import FlightConfirmation from './FlightConfirmation'
 
-export default ({ confirmSubmit, open, flight }) => {
-  const actions = [
-    <Button
-      key={1}
-      label="OK"
-      color='primary'
-      onClick={confirmSubmit}
-    />
-  ]
+export interface SignUpConfirmationProps {
+  confirmSubmit: () => void
+  open: boolean
+  flight: {}
+}
 
+export const SignUpConfirmation = ({ confirmSubmit, open, flight }: SignUpConfirmationProps) => {
   return (
     <div>
       <Dialog
         open={open}
-        actions={actions}
-        modal={true}
         title={'Traveler submitted'}>
         {
           flight && Object.keys(flight).length
             ? <FlightConfirmation flight={flight}/>
             : <h4>Sorry, we could not find your flight or it has already landed.</h4>
         }
+        <DialogActions>
+          <Button onClick={confirmSubmit} color='primary'>
+          Ok
+          </Button>
+        </DialogActions>
       </Dialog>
     </div>
   )
