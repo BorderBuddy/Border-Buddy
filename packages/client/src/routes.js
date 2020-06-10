5c399fe28c4091995344574981c811182f5ca5c7
thea rae <tr@thearae.com>
2020-06-10 10:55:43 -0400

    actually fixed and understand hot reloading

--- packages/client/src/routes.js
+++ packages/client/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 
 // Components
+import { Homepage } from './containers/Homepage'
 import AdminContainer from './containers/AdminContainer'
 import AllTravelers from './containers/AllTravelers'
 import SingleTravelerContainer from './containers/SingleTravelerContainer'
@@ -24,23 +25,23 @@ import {
 } from './utils/hooks'
 
 export const getRoutes = () => (
-    <Switch>   
-      <Route path="/why" component={WhyBorderBuddy}/>
-      <Route path="/register" component={ConnectedSignUpContainer}/>
-      <Route path="/about" component={About}/>
-      <Route component={WhyBorderBuddy}/>
-      {/* <Route path="success" component={Success} onEnter={onSuccessEnter} onLeave={onSuccessLeave} /> */}
-      {/* <Route path="/admin" component={AdminContainer} onEnter={onAdminEnter}>
-      <IndexRedirect to="/admin/travelers" />
-      <Route path="travelers" component={AllTravelers} onEnter={onTravelersListEnter} />
-      <Route path="travelers/add" component={AddTravelerContainer} />
-      <Route path="travelers/:id" component={SingleTravelerContainer} onEnter={onSingleTravelerEnter} />
-      <Route path="createuser" component={AdminSignUp} />
-      <Route path="updateprofile" component={UpdateUserContainer} />
-      </Route>
-      <Route path="/login" component={Login} /> */}
-    </Switch>
-  
+  <Switch>
+    <Route exact path="/why" component={WhyBorderBuddy}/>
+    <Route exact path="/register" component={ConnectedSignUpContainer}/>
+    <Route exact path="/about" component={About}/>
+    <Route component={WhyBorderBuddy} />
+
+    {/* <Route path="success" component={Success} onEnter={onSuccessEnter} onLeave={onSuccessLeave} /> */}
+    {/* <Route path="/admin" component={AdminContainer} onEnter={onAdminEnter}>
+    <IndexRedirect to="/admin/travelers" />
+    <Route path="travelers" component={AllTravelers} onEnter={onTravelersListEnter} />
+    <Route path="travelers/add" component={AddTravelerContainer} />
+    <Route path="travelers/:id" component={SingleTravelerContainer} onEnter={onSingleTravelerEnter} />
+    <Route path="createuser" component={AdminSignUp} />
+    <Route path="updateprofile" component={UpdateUserContainer} />
+    </Route>
+    <Route path="/login" component={Login} /> */}
+  </Switch>
 )
 
 // const App = () => (
