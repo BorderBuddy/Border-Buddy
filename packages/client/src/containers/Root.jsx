8a356f93e3924b7773c46b5336ac9088973c5c40
thea rae <tr@thearae.com>
2020-06-09 21:54:03 -0400

    idk but it stopped rendering the context and the url is still broken

--- packages/client/src/containers/Root.jsx
+++ packages/client/src/containers/Root.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import {Router as BrowserRouter } from 'react-router-dom'
 import { getRoutes } from '../routes'
+import { Footer } from '../components/Footer'
+import { Navbar } from '../components/Navbar'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import muiTheme from '../utils/muiTheme'
 
@@ -9,9 +11,22 @@ import muiTheme from '../utils/muiTheme'
 export const Root = ({ store }) => (
   <Provider store={store}>
     <MuiThemeProvider theme={muiTheme}>
+      <div id="banner" className="col-12">
+        <img style={styles.image} src="images/logos-png/BB_Logo-Type-White.png" />
+      </div>
+      <Navbar/>
       <Router>
         {getRoutes()}
       </Router>
+      <Footer/>
     </MuiThemeProvider>
   </Provider>
 )
+
+const styles = {
+  image: {
+    display: 'block',
+    margin: 'auto',
+    width: '50%'
+  }
+}
