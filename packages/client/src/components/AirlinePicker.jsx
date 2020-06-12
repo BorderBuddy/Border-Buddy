e244ee22a33a471771f8454c8b492ee65dfc1511
thea rae <tr@thearae.com>
2020-06-12 15:29:20 -0400

    fixed airport code input but not working due to api connection

--- packages/client/src/components/AirlinePicker.jsx
+++ packages/client/src/components/AirlinePicker.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {TextField} from '@material-ui/core';
 
-export default ({ input, label, style, hintStyle, underlineFocusStyle, errorStyle, type, meta: { asyncValidating, touched, error }}) => (
-    <div className={asyncValidating ? 'async-validating' : ''}>
-      <TextField
-        {...input}
-        type={type}
-        hintText={label}
-        style={style}
-        hintStyle={hintStyle}
-        underlineFocusStyle={underlineFocusStyle}
-        errorStyle={errorStyle}
-        errorText={touched && error && `${error}`} />
-    </div>
-);
+export const RenderAirlinePicker = ({ 
+  input, 
+  label, 
+  meta: { 
+    asyncValidating, 
+    touched, 
+    error, 
+    invalid},
+    ...custom
+  }) => {
+    return (
+    <TextField
+      className={asyncValidating ? 'async-validating' : ''}
+      name={name}
+      {...input}
+      {...custom}
+      label={label}
+      helperText={touched && error}
+      error={touched && invalid} />
+)}
