94fc91305b234afb2b464d1d5dbf87318a4a1964
thea rae <tr@thearae.com>
2020-06-12 12:52:23 -0400

    fixed textfield inputs on main form

--- packages/client/src/containers/Root.jsx
+++ packages/client/src/containers/Root.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 import { getRoutes } from '../routes'
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
-import muiTheme from '../utils/muiTheme'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import customTheme from '../utils/muiTheme'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 // eslint-disable-next-line react/prop-types
 export const Root = ({ store }) => (
-  <Provider store={store}>
-    <MuiThemeProvider theme={muiTheme}>
+  <MuiThemeProvider theme={customTheme}>
+    <Provider store={store}>
       <BrowserRouter>
         {/* <Navbar/> */}
         {getRoutes()}
         {/* <Footer/> */}
       </BrowserRouter>
-    </MuiThemeProvider>
-  </Provider>
+    </Provider>
+  </MuiThemeProvider>
 )
