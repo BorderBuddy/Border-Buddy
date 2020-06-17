8bd47afd4df7e33a3dac5f95c8af43c4c4aa9a2a
thea rae <tr@thearae.com>
2020-06-17 14:37:51 -0400

    fixed admin app bar and navigation links

--- packages/client/src/components/Admin/AppBar.jsx
+++ packages/client/src/components/Admin/AppBar.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
-import { useHistory as history } from 'react-router';
-import {IconMenu, IconButton, MenuItem} from '@material-ui/core';
-import {MoreVert} from '@material-ui/icons';
+import React, { Fragment } from "react"
+import { useHistory as history } from "react-router"
+import { Menu, IconButton, MenuItem } from "@material-ui/core"
+import { MoreVert } from "@material-ui/icons"
 
-export const AppBar = ({onSignoutClick}) => {
+const style = {
+  icon: {
+    color: "white",
+  },
+}
+
+export default function RenderAppBar({ onSignoutClick }) {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = (url) => {
+    setAnchorEl(null)
+    if (url === 'onSignoutClick') {
+      onSignoutClick()
+    } else if (url !== null || url !== undefined) {
+      history.push(url)
+    }
+  }
+
   return (
-    <IconMenu
-      iconButtonElement={
-        <IconButton>
-            <MoreVert color={'white'}/>
-          </IconButton>
-      }
-      targetOrigin={{horizontal: 'right', vertical: 'top'}}
-      anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-    >
-      <MenuItem primaryText="All Travelers" onClick={() => history.push('/admin/travelers')} />
-      <MenuItem primaryText="Create New User" onClick={() => history.push('/admin/createuser')}/>
-      <MenuItem primaryText="Update Profile" onClick={() => history.push('/admin/updateprofile')} />
-      <MenuItem primaryText="Sign out" onClick={onSignoutClick}/>
-    </IconMenu>
+    <Fragment>
+      <IconButton
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVert style={style.icon} />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose} >
+        <MenuItem onClick={() => handleClose("/admin/travelers")}>
+          All Travelers
+        </MenuItem>
+        <MenuItem onClick={() => handleClose("/admin/createuser")}>
+          Create New User
+        </MenuItem>
+        <MenuItem onClick={() => handleClose("/admin/updateprofile")}>
+          Update Profile
+        </MenuItem>
+        <MenuItem onClick={() => handleClose("onSignoutClick")}>
+          Sign out
+        </MenuItem>
+      </Menu>
+    </Fragment>
   )
 }
--- packages/client/src/containers/AdminSignUpContainer.jsx
+++ packages/client/src/containers/AdminSignUpContainer.jsx
@@ -1,95 +1,95 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import AdminSignUp from "../components/Admin/AdminSignUp";
-import { signup, updateUser } from '../actions/auth';
-import {Dialog, Button} from '@material-ui/core';
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import AdminSignUp from "../components/Admin/AdminSignUp"
+import { signup, updateUser } from "../actions/auth"
+import { Dialog, Button } from "@material-ui/core"
 
 class AdminSignUpContainer extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props)
 
-		this.state = {
-			email: '',
-			password: '',
-			phone: '',
-			open: false
-		}
-		this.handleEmailChange = this.handleEmailChange.bind(this);
-		this.handlePasswordChange = this.handlePasswordChange.bind(this);
-		this.handlePhoneChange = this.handlePhoneChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleClose = this.handleClose.bind(this);
-	}
+    this.state = {
+      email: "",
+      password: "",
+      phone: "",
+      open: false,
+    }
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.handlePhoneChange = this.handlePhoneChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+  }
 
-	handleEmailChange(e) {
-		this.setState({ email: e.target.value })
-	}
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value })
+  }
 
-	handlePasswordChange(e) {
-		this.setState({ password: e.target.value })
-	}
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value })
+  }
 
-	handlePhoneChange(e) {
-		this.setState({ phone: e.target.value })
-	}
+  handlePhoneChange(e) {
+    this.setState({ phone: e.target.value })
+  }
 
-	handleSubmit(e) {
-		e.preventDefault();
-		this.props.signup({
-			email: this.state.email,
-			password: this.state.password,
-			phone: this.state.phone
-		})
-		.then(() => {
-			this.setState({ open: true, createdSuccess: true })
-		})
-		.catch(() => {
-			this.setState({ open: true, createdSuccess: false })
-		})
-	}
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props
+      .signup({
+        email: this.state.email,
+        password: this.state.password,
+        phone: this.state.phone,
+      })
+      .then(() => {
+        this.setState({ open: true, createdSuccess: true })
+      })
+      .catch(() => {
+        this.setState({ open: true, createdSuccess: false })
+      })
+  }
 
-	handleClose() {
-		this.setState({ open: false })
-	}
+  handleClose() {
+    this.setState({ open: false })
+  }
 
-	render() {
-		let actions = [
-			<Button
-				label="OK"
-				variant='contained'
-            	color='primary'
-				onClick={this.handleClose}
-			/>
-		]
+  render() {
+    let actions = [
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={this.handleClose}
+      >OK</Button>,
+    ]
 
-		return(
-			<div>
-				<AdminSignUp 
-					handleEmailChange={this.handleEmailChange}
-					handlePasswordChange={this.handlePasswordChange}
-					handlePhoneChange={this.handlePhoneChange}
-					handleSubmit={this.handleSubmit} />
-				<Dialog
-					title="Creating New Admin..."
-					actions={actions}
-					open={this.state.open}
-					modal={true}
-				>
-					{
-						this.state.createdSuccess ?
-							<h4>New admin created successfully!</h4>
-							:
-							<h4>There was a problem trying to create a new admin</h4>
-					}
-				</Dialog>
-			</div>
-		)
-	}
+    return (
+      <div>
+        <AdminSignUp
+          handleEmailChange={this.handleEmailChange}
+          handlePasswordChange={this.handlePasswordChange}
+          handlePhoneChange={this.handlePhoneChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <Dialog
+          title="Creating New Admin..."
+          actions={actions}
+          open={this.state.open}
+          modal={true}
+        >
+          {this.state.createdSuccess ? (
+            <h4>New admin created successfully!</h4>
+          ) : (
+            <h4>There was a problem trying to create a new admin</h4>
+          )}
+        </Dialog>
+      </div>
+    )
+  }
 }
 
-const mapDispatchToProps = dispatch => ({
-	signup: (user) => dispatch(signup(user)),
-	updateUser: (user) => dispatch(updateUser(user))
+const mapDispatchToProps = (dispatch) => ({
+  signup: (user) => dispatch(signup(user)),
+  updateUser: (user) => dispatch(updateUser(user)),
 })
 
-export default connect(null, mapDispatchToProps)(AdminSignUpContainer);
+export default connect(null, mapDispatchToProps)(AdminSignUpContainer)
