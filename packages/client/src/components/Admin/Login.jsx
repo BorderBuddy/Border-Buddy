b210700cca98cb055c234742cebc5d14d1b7c446
thea rae <tr@thearae.com>
2020-06-17 14:37:51 -0400

    fixed admin app bar and navigation links

--- packages/client/src/components/Admin/Login.jsx
+++ packages/client/src/components/Admin/Login.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { reduxForm, Field } from 'redux-form'
 import { RenderTextField } from '../Field'
-import { Link } from 'react-router-dom'
 import { Card, Button } from '@material-ui/core'
 import { signupLoginStyle, formStyle } from './styles'
 
@@ -10,7 +9,6 @@ const required = value => (value == null ? 'Required' : undefined)
 const AdminLogin = ({
   handleSubmit,
   pristine,
-  reset,
   submitting,
   handleEmailChange,
   handlePasswordChange,
@@ -20,38 +18,36 @@ const AdminLogin = ({
 
   return (
     <Card style={style.card}>
-      <div>
-        <h3 style={style.title}>Admin Login</h3>
-        <form onSubmit={handleSubmit}>
-          <Field
-            name="email"
-            component={RenderTextField}
-            label="Email"
-            validate={required}
-            onChange={handleEmailChange}
-            style={formStyle.input}
-          />
-          <Field
-            name="password"
-            type="password"
-            component={RenderTextField}
-            label="Password"
-            validate={required}
-            onChange={handlePasswordChange}
-            style={formStyle.input}
-          />
-          {auth.fetching && <p style={style.loader}>Logging In...</p>}
-          {auth.error && <p style={style.error}>ERROR: {auth.error.message}</p>}
-          <Button
-            type="submit"
-            label="Login"
-            disabled={pristine || submitting}
-            variant='contained'
-            color='primary'
-            style={formStyle.input}
-          > Login </Button>
-        </form>
-      </div>
+      <h3 style={style.title}>Admin Login</h3>
+      <form onSubmit={handleSubmit}>
+        <Field
+          name="email"
+          component={RenderTextField}
+          label="Email"
+          validate={required}
+          onChange={handleEmailChange}
+          style={formStyle.input}
+        />
+        <Field
+          name="password"
+          type="password"
+          component={RenderTextField}
+          label="Password"
+          validate={required}
+          onChange={handlePasswordChange}
+          style={formStyle.input}
+        />
+        {auth.fetching && <p style={style.loader}>Logging In...</p>}
+        {auth.error && <p style={style.error}>ERROR: {auth.error.message}</p>}
+        <Button
+          type="submit"
+          label="Login"
+          disabled={pristine || submitting}
+          variant='contained'
+          color='primary'
+          style={formStyle.input}
+        > Login </Button>
+      </form>
     </Card>
   )
 }
