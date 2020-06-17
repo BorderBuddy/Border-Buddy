a79d9668b33cb88964e073ebb312863046908755
thea rae <tr@thearae.com>
2020-06-17 14:37:51 -0400

    fixed admin app bar and navigation links

--- packages/client/src/containers/AdminContainer.jsx
+++ packages/client/src/containers/AdminContainer.jsx
@@ -1,38 +1,42 @@
 import React, { Component } from 'react'
 import { useHistory as history, Link } from 'react-router-dom'
-import { Toolbar, ToolbarGroup, ToolbarTitle, IconButton, Button } from '@material-ui/core'
+import { Toolbar, ToolbarGroup, ToolbarTitle, IconButton, Button , AppBar} from '@material-ui/core'
 import {ActionFlightTakeoff} from '@material-ui/icon'
-import AppBarMenu from './AppBarMenu'
+import RenderAppBarMenu from './AppBarMenu'
 
 const style = {
   button: {
     margin: '2em auto',
     display: 'block',
-    width: '50%'
+    width: '50%',
+    color: 'white'
+  },
+  icon: {
+    maxHeight: '1.5em'
   }
 }
 
 export default class AdminContainer extends Component {
   render () {
     return (
-      <div>
+      <AppBar>
         <Toolbar style={{ backgroundColor: '#2d6ea8' }}>
           <IconButton
-            style={style}
             id="btn-all-travelers"
             onClick={() => history.push('/admin/travelers')}>
-            <img src="/images/logos-png/BB_Logo_03-White.png" />
+            <img src="/images/logos-png/BB_Logo_03-White.png" style={style.icon}/>
           </IconButton>
           <Button
-            style={style}
+            style={style.button}
             id="add-new-traveler"
+            varitant='text'
             onClick={() => history.push('/admin/travelers/add')}>
               Add Traveler
           </Button>
-          <AppBarMenu/>
+          <RenderAppBarMenu/>
         </Toolbar>
-        {/* {this.props.children} */}
-      </div>
+        {this.props.children}
+      </AppBar>
     )
   }
 }
