8ddbb7d9a9f398c7ac51f1a28e50f78fcf056e94
thea rae <tr@thearae.com>
2020-06-19 10:55:34 -0400

    admin sign up rendering

--- packages/client/src/components/Admin/AdminSignUp.jsx
+++ packages/client/src/components/Admin/AdminSignUp.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
-import { TextField } from 'redux-form-material-ui'
-import { Card, Button } from '@material-ui/core'
+import { RenderTextField } from '../Field'
+import { Card, Button, Typography } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { signupLoginStyle } from './styles'
+import { adminSignUp } from './styles'
 import { minimumLength, required, phone } from '../../utils/validations'
 
 class AdminSignUp extends Component {
   render () {
-    const style = signupLoginStyle
+    const style = adminSignUp
     const {
       handleSubmit, pristine, submitting,
       handleEmailChange, handlePasswordChange, handlePhoneChange, valid
@@ -22,37 +22,37 @@ class AdminSignUp extends Component {
           <form onSubmit={handleSubmit}>
             <Field
               name="email"
-              component={TextField}
-              hintText="Email"
+              component={RenderTextField}
+              label="Email"
               validate={required}
               onChange={handleEmailChange}
-              style={style.form}
+              style={style.input}
             />
             <Field
               name="password"
-              type="password"
-              component={TextField}
-              hintText="Password (at least 8 characters)"
+              type='password'
+              component={RenderTextField}
+              label="Password"
               validate={[required, minimumLength]}
               onChange={handlePasswordChange}
-              style={style.form}
+              style={style.input}
             />
+            <Typography variant='caption'>Minimum 8 charaters</Typography>
             <Field
               name="phone"
-              component={TextField}
-              hintText="Phone Number"
+              component={RenderTextField}
+              label="Phone Number"
               validate={[phone]}
               onChange={handlePhoneChange}
-              style={style.form}
+              style={style.input}
             />
             <Button
               type="submit"
-              label="Sign Up"
               disabled={pristine || submitting || !valid}
               color='primary'
               variant='contained'
               style={style.button}
-            />
+            >Sign Up</Button>
           </form>
         </div>
       </Card>
