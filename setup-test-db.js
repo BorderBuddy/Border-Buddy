8ed9b2176bf35b4f01072ecfd82db210ff9e0fa4
Aaron Arntz <aarntz@gmail.com>
2020-06-19 12:53:30 -0400

    try without setup

--- setup-test-db.js
+++ setup-test-db.js
@@ -10,21 +10,3 @@ const pgclient = new Client({
 
 pgclient.connect()
 pgclient.end()
-
-// const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-// const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-// const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
-
-// pgclient.query(table, (err, res) => {
-// if (err) throw err
-// })
-
-// pgclient.query(text, values, (err, res) => {
-// if (err) throw err
-// })
-
-// pgclient.query('SELECT * FROM student', (err, res) => {
-// if (err) throw err
-// console.log(err, res.rows) // Print the data in student table
-// pgclient.end()
-// })
