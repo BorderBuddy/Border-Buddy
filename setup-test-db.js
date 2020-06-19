c0f5681e7e585ef5987abaa8e46f419816847767
Aaron Arntz <aarntz@gmail.com>
2020-06-19 12:45:23 -0400

    test db connection

--- setup-test-db.js
+++ setup-test-db.js
@@ -9,6 +9,7 @@ const pgclient = new Client({
 })
 
 pgclient.connect()
+pgclient.end()
 
 // const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
 // const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
