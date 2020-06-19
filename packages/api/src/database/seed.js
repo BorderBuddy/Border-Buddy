75ed05ca2c26ce76a84c91fd51d319919224bb45
Aaron Arntz <aarntz@gmail.com>
2020-06-19 12:53:30 -0400

    try without setup

--- packages/api/src/database/seed.js
+++ packages/api/src/database/seed.js
@@ -11,7 +11,7 @@ const threeHoursAgo = new Date(new Date() - 1000 * 60 * 60 * 3)
 const users = [
   { email: 'andrew@borderbuddy.us', password: '12345678', phone: '1234567890' },
   { email: 'dillon@borderbuddy.us', password: '12345678', phone: '1234567890' },
-  { email: 'aaron@borderbuddy.us', password: 'password', phone: '3233655969' },
+  { email: 'aaron@borderbuddy.us', password: '12345678', phone: '1234567890' },
   { email: 'admin@borderbuddy.us', password: '12345678', phone: '1234567890' }
 ]
 
@@ -39,16 +39,16 @@ const dummyFlights = [{
 ]
 
 const dummyTravelers = [
-  { email: '1234@abc.com', countryCode: '1', phone: '8084982097', name: 'Dirron Pewers', status: 'transit', connectivity: true, nationality: 'Syria', flightId: 1 },
-  { email: '3456@abc.com', countryCode: '93', phone: '8084982097', name: 'Gandrew Aionfriddy', status: 'transit', connectivity: true, nationality: 'Yemen', flightId: 1 },
-  { email: 'diap@abc.com', countryCode: '98', phone: '8084982097', name: 'Emily Whasser', status: 'transit', connectivity: true, nationality: 'Iran', flightId: 1 },
-  { email: 'tafman@abc.com', countryCode: '964', phone: '8084982097', name: 'Taf Thaman', status: 'transit', connectivity: true, nationality: 'Iraq', flightId: 1 },
-  { email: 'ianb@abc.com', countryCode: '1', phone: '8084982097', name: 'Ian Munrovia', status: 'transit', connectivity: true, nationality: 'Afghanistan', flightId: 1 },
-  { email: 'peeplesm@abc.com', countryCode: '963', phone: '8084982097', name: 'Peeples McPerson', status: 'transit', connectivity: true, nationality: 'Afghanistan', flightId: 1 },
-  { email: 'tatiiii@abc.com', countryCode: '93', phone: '8084982097', name: 'Tatiana Alex', status: 'transit', connectivity: true, nationality: 'Yemen', flightId: 1 },
-  { email: 'clemm@abc.com', countryCode: '92', phone: '8084982097', name: 'Wat Uwotmate', status: 'transit', connectivity: true, nationality: 'Nigeria', flightId: 1 },
-  { email: 'anthony@abc.com', countryCode: '966', phone: '8084982097', name: 'DT Jacksern', status: 'unconfirmed', connectivity: true, nationality: 'Sudan', flightId: 1 },
-  { email: 'melbrux@abc.com', countryCode: '1', phone: '8084982097', name: 'Fraulein Hhauserr', status: 'unconfirmed', connectivity: true, nationality: 'Sudan', flightId: 1 }
+  { email: '1234@abc.com', countryCode: '1', phone: '1234567890', name: 'Dirron Pewers', status: 'transit', connectivity: true, nationality: 'Syria', flightId: 1 },
+  { email: '3456@abc.com', countryCode: '93', phone: '1234567890', name: 'Gandrew Aionfriddy', status: 'transit', connectivity: true, nationality: 'Yemen', flightId: 1 },
+  { email: 'diap@abc.com', countryCode: '98', phone: '1234567890', name: 'Emily Whasser', status: 'transit', connectivity: true, nationality: 'Iran', flightId: 1 },
+  { email: 'tafman@abc.com', countryCode: '964', phone: '1234567890', name: 'Taf Thaman', status: 'transit', connectivity: true, nationality: 'Iraq', flightId: 1 },
+  { email: 'ianb@abc.com', countryCode: '1', phone: '1234567890', name: 'Ian Munrovia', status: 'transit', connectivity: true, nationality: 'Afghanistan', flightId: 1 },
+  { email: 'peeplesm@abc.com', countryCode: '963', phone: '1234567890', name: 'Peeples McPerson', status: 'transit', connectivity: true, nationality: 'Afghanistan', flightId: 1 },
+  { email: 'tatiiii@abc.com', countryCode: '93', phone: '1234567890', name: 'Tatiana Alex', status: 'transit', connectivity: true, nationality: 'Yemen', flightId: 1 },
+  { email: 'clemm@abc.com', countryCode: '92', phone: '1234567890', name: 'Wat Uwotmate', status: 'transit', connectivity: true, nationality: 'Nigeria', flightId: 1 },
+  { email: 'anthony@abc.com', countryCode: '966', phone: '1234567890', name: 'DT Jacksern', status: 'unconfirmed', connectivity: true, nationality: 'Sudan', flightId: 1 },
+  { email: 'melbrux@abc.com', countryCode: '1', phone: '1234567890', name: 'Fraulein Hhauserr', status: 'unconfirmed', connectivity: true, nationality: 'Sudan', flightId: 1 }
 ]
 
 const seedFlights = () => Promise.each(dummyFlights, flight => db.model('flight').create(flight))
