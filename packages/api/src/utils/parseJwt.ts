c215b1b0de90436dd6539dc5c41ab610e9e57358
Aaron Arntz <aarntz@gmail.com>
2020-06-25 13:19:47 -0400

    fix /user/me api route

---
+++ packages/api/src/utils/parseJwt.ts
@@ -0,0 +1,9 @@
+const atob = (a: string) => Buffer.from(a, 'base64').toString('binary')
+export const parseJwt = (token: string) => {
+  const base64Url = token.split('.')[1]
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+  const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+  }).join(''))
+  return JSON.parse(jsonPayload)
+}
