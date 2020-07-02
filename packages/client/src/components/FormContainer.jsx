bafb7c6e2bb47bf12a851c21dc0bfca0cb398263
Aaron Arntz <aarntz@gmail.com>
2020-07-02 13:32:03 -0400

    establish formik format for register form

--- packages/client/src/components/FormContainer.jsx
+++ packages/client/src/components/FormContainer.jsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import Form from './Form';
-import { reduxForm } from 'redux-form';
-import { validateCode } from '../utils/validations';
+import React, { Component } from 'react'
+import Form from './Form'
+import { reduxForm } from 'redux-form'
+import { validateCode } from '../utils/validations'
 
 class FormContainer extends Component {
-
-  componentDidMount() {
-    this.props.initialize();
+  componentDidMount () {
+    this.props.initialize()
   }
 
-  render() {
+  render () {
     return <Form {...this.props} />
   }
 }
 
 export default reduxForm({
-  form: 'travelerForm',  // a unique identifier for this form
+  form: 'travelerForm', // a unique identifier for this form
   asyncValidate: validateCode,
   asyncBlurFields: ['airlineCode']
-})(FormContainer);
+})(FormContainer)
