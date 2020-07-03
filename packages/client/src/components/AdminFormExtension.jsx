c193af85f7b2b26abe1bb0d0e5f09d55b9b249aa
thea rae <tr@thearae.com>
2020-07-03 08:58:31 -0400

    log out working

--- packages/client/src/components/AdminFormExtension.jsx
+++ packages/client/src/components/AdminFormExtension.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { TextField, DatePicker, SelectField } from 'redux-form-material-ui';
-import { MenuItem} from '@material-ui/core';
-import { Field } from 'redux-form';
-import { adminFormExtensionStyle } from './styles';
+import React from "react"
+import { TextField, DatePicker, SelectField } from "redux-form-material-ui"
+import { MenuItem } from "@material-ui/core"
+import { Field } from "redux-form"
+import { adminFormExtensionStyle } from "./styles"
 
 export default (props) => {
-
   const menuItems = (representatives) => {
     return (
-      representatives && representatives.map((rep, index) => {
-        return <MenuItem key={index} value={rep.id} primaryText={first3Chars(rep.email)} className="traveler-assign-to-option" />;
+      representatives &&
+      representatives.map((rep, index) => {
+        return (
+          <MenuItem
+            key={index}
+            value={rep.id}
+            primaryText={first3Chars(rep.email)}
+            className="traveler-assign-to-option"
+          />
+        )
       })
-    );
-  };
+    )
+  }
 
   const first3Chars = (text) => {
-    return text.substring(0, 3).toUpperCase();
-  };
-  
+    return text.substring(0, 3).toUpperCase()
+  }
+
   return (
     <div>
       <h3>Additional Administrative Fields</h3>
-      <p><em>Additional fields for administrative purposes.</em></p>
+      <p>
+        <em>Additional fields for administrative purposes.</em>
+      </p>
       <div className="field-container col-12 md-col md-col-6">
         <Field
           name="flightStatus"
@@ -33,9 +42,9 @@ export default (props) => {
           underlineFocusStyle={adminFormExtensionStyle.underline}
           disabled={true}
         >
-          <MenuItem value={'scheduled'} primaryText="Scheduled"/>
-          <MenuItem value={'delayed'} primaryText="Delayed"/>
-          <MenuItem value={'arrived'} primaryText="Arrived"/>
+          <MenuItem value={"scheduled"} primaryText="Scheduled" />
+          <MenuItem value={"delayed"} primaryText="Delayed" />
+          <MenuItem value={"arrived"} primaryText="Arrived" />
         </Field>
       </div>
       <div className="field-container col-12 md-col md-col-6">
@@ -48,11 +57,11 @@ export default (props) => {
           hintStyle={adminFormExtensionStyle.label}
           underlineFocusStyle={adminFormExtensionStyle.underline}
         >
-          <MenuItem value={'transit'} primaryText="In Transit"/>
-          <MenuItem value={'unconfirmed'} primaryText="Unconfirmed"/>
-          <MenuItem value={'detained'} primaryText="Detained"/>
-          <MenuItem value={'at risk'} primaryText="At Risk"/>
-          <MenuItem value={'cleared'} primaryText="Cleared"/>
+          <MenuItem value={"transit"} primaryText="In Transit" />
+          <MenuItem value={"unconfirmed"} primaryText="Unconfirmed" />
+          <MenuItem value={"detained"} primaryText="Detained" />
+          <MenuItem value={"at risk"} primaryText="At Risk" />
+          <MenuItem value={"cleared"} primaryText="Cleared" />
         </Field>
       </div>
       <div className="field-container col-12 md-col md-col-6">
