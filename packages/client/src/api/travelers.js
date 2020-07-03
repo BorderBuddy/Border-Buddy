8248bfa3189154333b8e03d4f0d6794b66da1e22
thea rae <tr@thearae.com>
2020-07-02 23:48:35 -0400

    small tweaks

--- packages/client/src/api/travelers.js
+++ packages/client/src/api/travelers.js
@@ -1,46 +1,49 @@
-import axios from 'axios'
+import axios from "axios"
 
 const api = {
-	getTraveler: async (id) => {
-		try{
-				const res = await axios.get(`/api/traveler/${id}`)
-				return res.data
-		} catch ({res}){
-				return res.data
-		}
-	},
-	updateTraveler: async (id, travelerAttributes) => {
-			try{
-					const res = await axios.put(`/api/traveler/${id}/update`, travelerAttributes)
-					return res.data
-			} catch ({res}){
-					return res.data
-			}
-	},
-	createTraveler: async (travelerAttributes) => {
-			try{
-					const res = await axios.post(`/api/traveler/create`, travelerAttributes)
-					return res.data
-			} catch ({res}){
-					return res.data
-			}
-	},
-	getTravelers: async () => {
-			try{
-					const res = await axios.get(`/api/travelers`)
-					return res.data
-			} catch ({res}){
-					return res.data
-			}
-	},
-	getTravelersByLawyer: async (id) => {
-		try{
-				const res = await axios.get(`/api/travelers/lawyer/${id}`)
-				return res.data
-		} catch ({res}){
-				return res.data
-		}
-},
+  getTraveler: async (id) => {
+    try {
+      const res = await axios.get(`/api/traveler/${id}`)
+      return res.data
+    } catch ({ res }) {
+      return res.data
+    }
+  },
+  updateTraveler: async (id, travelerAttributes) => {
+    try {
+      const res = await axios.put(
+        `/api/traveler/${id}/update`,
+        travelerAttributes
+      )
+      return res.data
+    } catch ({ res }) {
+      return res.data
+    }
+  },
+  createTraveler: async (travelerAttributes) => {
+    try {
+      const res = await axios.post(`/api/traveler/create`, travelerAttributes)
+      return res.data
+    } catch ({ res }) {
+      return res.data
+    }
+  },
+  getTravelers: async () => {
+    try {
+      const res = await axios.get(`/api/travelers`)
+      return res.data
+    } catch ({ res }) {
+      return res.data
+    }
+  },
+  getTravelersByLawyer: async (id) => {
+    try {
+      const res = await axios.get(`/api/travelers/lawyer/${id}`)
+      return res.data
+    } catch ({ res }) {
+      return res.data
+    }
+  },
 }
 
 export default api
