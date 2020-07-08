b8b4d774521386d072497053310de99755d8d8fa
Aaron Arntz <aarntz@gmail.com>
2020-07-08 12:09:44 -0400

    put tests back to working

--- packages/client/src/actions/auth.js
+++ packages/client/src/actions/auth.js
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from 'axios'
 import {
   SET_AUTH,
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
-  LOGOUT,
-} from "../constants"
+  LOGOUT
+} from '../constants'
 
 export const setAuth = (auth) => ({ type: SET_AUTH, auth })
 export const loginRequest = () => ({
@@ -42,15 +42,37 @@ export const logout = () => ({
   error: false
 })
 
-export const login = (res) => (dispatch) => {
-  console.log(`in auth actions ${res}`)
+// TODO: This doesn't hit api, just dispatches redux actions
+// export const login = (res) => (dispatch) => {
+//   console.log(`in auth actions ${res}`)
+//   dispatch(loginRequest())
+//   dispatch(loginSuccess(res))
+// }
+
+export const login = (email, password) => (dispatch) => {
   dispatch(loginRequest())
-  dispatch(loginSuccess(res))
+  return axios
+    .post('/api/auth/local', { email, password })
+    .then((res) => {
+      console.log('just logged in and setting the token')
+      window.localStorage.setItem('accessToken', res.data.token)
+      dispatch(loginSuccess(res.data))
+      return res.status
+    })
+    .catch((err) => {
+      console.log('there was an error loging in: ' + err)
+      dispatch(loginFailure(err.res.data.message))
+      return err
+    })
 }
 
 export const signup = (user, _window = window) => () => {
   return axios
-    .post('/api/user', user)
+    .post('/api/user', user, {
+      headers: {
+        Authorization: _window.localStorage.accessToken
+      }
+    })
     .catch((err) => console.error('ERROR!', err))
 }
 
