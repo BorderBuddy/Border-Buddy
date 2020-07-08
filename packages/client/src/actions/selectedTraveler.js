43058f8fd94fa941d5857649aa7ba146cc43127a
Aaron Arntz <aarntz@gmail.com>
2020-07-08 12:09:44 -0400

    put tests back to working

--- packages/client/src/actions/selectedTraveler.js
+++ packages/client/src/actions/selectedTraveler.js
@@ -6,20 +6,20 @@ export const setSelectedTraveler = selectedTraveler => ({ type: SET_SELECTED_TRA
 
 export const fetchSelectedTraveler = (id) => dispatch => {
   return axios.get(`/api/traveler/${id}`)
-  .then(traveler => {
+    .then(traveler => {
       traveler = traveler.data
       dispatch(setSelectedTraveler(traveler))
       dispatch(setFlight(traveler.flight || {}))
-  })
-  .catch(err => console.error(err))
-
-  //for offline dev
+    })
+    .catch(err => console.error(err))
+  // for offline dev
   // const traveler = { id:'1231', email: '1234@abc.com', countryCode: '1', phone: '8084982097', name: 'Dirron Pewers', status: 'transit', connectivity: true, nationality: 'Syria', flightId: 1 }
   // return dispatch(setSelectedTraveler(traveler))
 }
-
 export const updateTraveler = (traveler, id, _window = window) => dispatch => {
-  return axios.put(`/api/traveler/${id}`, traveler)
+  return axios.put(`/api/traveler/${id}`, traveler, {
+    headers: { Authorization: _window.localStorage.accessToken }
+  })
     .then(response => {
       traveler = response.data
       dispatch(setSelectedTraveler(traveler))
@@ -32,15 +32,18 @@ export const sendText = (traveler, _window = window) => () => {
     {
       to: `+${traveler.countryCode}${traveler.phone}`,
       message: `Hi ${traveler.name}, we have not heard from you yet. Please respond with 'ok' if you are through customs and immigration.`
+    }, {
+      headers: { Authorization: _window.localStorage.accessToken }
     })
     .then((res) => {
       console.log('message sent!', res.data)
     })
     .catch(err => console.error(err))
 }
-
 export const deleteTraveler = (id, _window = window) => dispatch => {
-  return axios.delete(`/api/traveler/${id}`)
+  return axios.delete(`/api/traveler/${id}`, {
+    headers: { Authorization: _window.localStorage.accessToken }
+  })
     .then((data) => {
       console.log('***********traveler deleted*************', data)
     })
