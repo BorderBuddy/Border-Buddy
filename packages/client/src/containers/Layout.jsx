c61fac87f26797c11d0fda6440d0faaa081769ea
thea rae <tr@thearae.com>
2020-07-09 10:38:44 -0400

    fixed the appBar

--- packages/client/src/containers/Layout.jsx
+++ packages/client/src/containers/Layout.jsx
@@ -5,6 +5,7 @@ import { RenderAppBar } from '../components/Admin/AppBar'
 import api from '../api/api'
 import { LoggedIn, LoggedOut } from '../auth/authService'
 import { getRoutes } from '../routes'
+import { Toolbar, IconButton, Button, AppBar, Container } from '@material-ui/core'
 
 class Homepage extends Component {
   constructor(props) {
@@ -12,7 +13,8 @@ class Homepage extends Component {
     this.onSignoutClick = this.onSignoutClick.bind(this)
   }
 
-  async onSignoutClick() {
+  async onSignoutClick () {
+    console.log(this.props)
     try {
       await api.logout()
       this.props.history.push('/')
@@ -24,32 +26,67 @@ class Homepage extends Component {
   render () {
     return (
       <div id="homepage">
-        <div id="banner" className="col-12">
-          <img
-            style={styles.image}
-            src="images/logos-png/BB_Logo-Type-White.png"
-          />
-        </div>
-        <LoggedIn>
-          {/* Logged In */}
-          <RenderAppBar onSignoutClick={this.onSignoutClick} />
-        </LoggedIn>
-        <LoggedOut>
-          <Navbar />
-        </LoggedOut>
-        {getRoutes()}
+        <AppBar className="navbar col-12" position="sticky">
+          <LoggedIn>
+            <Toolbar style={styles.toolbar}>
+              <IconButton
+                id="btn-all-travelers"
+                onClick={() => this.props.history.push('/travelers')}>
+                <img src="/images/logos-png/BB_Logo_03-White.png" style={styles.icon}/>
+              </IconButton>
+              <Button
+                style={styles.button}
+                id="add-new-traveler"
+                varitant='text'
+                onClick={() => this.props.history.push('/travelers/add')}>
+                  Add Traveler
+              </Button>
+              <RenderAppBar onSignoutClick={this.onSignoutClick} />
+            </Toolbar>
+          </LoggedIn>
+          <LoggedOut>
+            <div id="banner" className="col-12">
+              <img
+                style={styles.image}
+                src="images/logos-png/BB_Logo-Type-White.png"
+              />
+            </div>
+            <Toolbar style={styles.toolbar}>
+              <Navbar />
+            </Toolbar>
+          </LoggedOut>
+        </AppBar>
+        <Container style={styles.container}>
+          {getRoutes()}
+        </Container>
         <Footer />
       </div>
     )
   }
 }
-
 const styles = {
-  image: {
-    display: "block",
-    margin: "auto",
-    width: "50%",
+  button: {
+    margin: '2em auto',
+    display: 'block',
+    width: '50%',
+    color: 'white'
   },
+  icon: {
+    maxHeight: '1.5em'
+  },
+  container: {
+    marginTop: '2em',
+    marginBottom: '4em'
+  },
+  toolbar: {
+    backgroundColor: '#2d6ea8'
+  },
+  image: {
+    display: 'block',
+    margin: 'auto',
+    width: '50%',
+    maxHeight: '2em'
+  }
 }
 
 export default Homepage
