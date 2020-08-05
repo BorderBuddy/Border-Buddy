efd32e8f9c82fa2c9e65bdfe83cf65e83cb7339e
thea rae <tr@thearae.com>
2020-08-04 20:52:52 -0400

    reverting brute

--- packages/api/src/auth/auth.route.js
+++ packages/api/src/auth/auth.route.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-new */
 import * as passportConfig from './passport'
 import { login, isAuthenticated, logout } from './auth.controller'
 import { User } from '../database/models/user'
 import sequelize from '../database/db'
 import ExpressBrute from 'express-brute'
-// import SequelizeStore from 'express-brute-sequelize'
-import ExpressBruteStore from 'express-brute-store-sequelize'
+import SequelizeStore from 'express-brute-sequelize'
+// import ExpressBruteStore from 'express-brute-store-sequelize'
 
 const base = '/api/auth'
 
@@ -15,32 +16,32 @@ export default (app) => {
   app.post(base + '/logout', logout)
 
   if (process.env.NODE_ENV === 'production') {
-    const bruteOptions = {
-      freeRetries: 3,
-      minWait: 5000,
-      attachResetToRequest: false,
-      refreshTimeoutOnRequest: false,
-    }
+    new SequelizeStore(sequelize, 'bruteStore', {}, (store) => {
+      const bruteforce = new ExpressBrute(store, {
+        freeRetries: 10,
+        minWait: 5 * 1000,
+        attachResetToRequest: false,
+        refreshTimeoutOnRequest: true,
+      })
 
-    const bruteStoreOptions = {
-      tableName: 'bruteStore', // this is a default name
-    }
-    const bruteStore = new ExpressBruteStore(sequelize, bruteStoreOptions)
-    const brute = new ExpressBrute(bruteStore, bruteOptions)
-    // SequelizeStore(sequelize, 'bruteStore', {}, (store) => {
-    //   const bruteforce = new ExpressBrute(store,
-    //     {
-    //       freeRetries: 3,
-    //       minWait: 5 * 1000,
-    //       attachResetToRequest: false,
-    //       refreshTimeoutOnRequest: true,
-    //     })
-
-    app.post(base + '/local',
-      brute.prevent,
-      login)
+      app.post(base + '/local',
+        bruteforce.prevent,
+        login)
+    })
   } else {
     app.post(base + '/local', login)
   }
 }
 
+//   const bruteOptions = {
+//     freeRetries: 3,
+//     minWait: 5000,
+//     attachResetToRequest: false,
+//     refreshTimeoutOnRequest: false,
+//   }
+
+//   const bruteStoreOptions = {
+//     tableName: 'bruteStore', // this is a default name
+//   }
+// const bruteStore = new ExpressBruteStore(sequelize, bruteStoreOptions)
+// const brute = new ExpressBrute(bruteStore, bruteOptions)
