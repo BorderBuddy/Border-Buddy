cdaa62d8b6d1f5a2b384d75029f2ecf3e273ea3d
thea rae <tr@thearae.com>
2020-09-11 15:39:26 -0400

    fixing up the css to remove the SASS

--- packages/client/src/components/static/About.jsx
+++ packages/client/src/components/static/About.jsx
@@ -1,39 +1,47 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
 
 export const About = () => (
-  <div className="clearfix tab-container about">
-    <div className="col-12">
+  <Grid container direction='row' justify='space-around'>
+    <Grid xs={12} justify='space-around'>
       <h2 className="mx-auto h1 center subtitle">About Us</h2>
-    </div>
-    <div className="col-12">
+    </Grid>
+    <Grid xs={10} justify='space-around'>
       <p className="center">We started at Hack the Ban.</p>
-      <a href="http://www.vocativ.com/405756/coders-hack-trump-immigration-ban/" target="_blank" rel="noopener noreferrer">
-        <img className="hacktheban-link" src="images/hacktheban.png" />
-      </a>
-
+      <Grid container direction='row' justify='space-around'>
+        <Grid item xs={12} md={5} justify='space-around'>
+          <a href="http://www.vocativ.com/405756/coders-hack-trump-immigration-ban/" target="_blank" rel="noopener noreferrer" >
+            <img src="images/hacktheban.png" style={{maxHeight: '100%', maxWidth: '100%'}}/>
+          </a>
+        </Grid>
+      </Grid>
       <p className="center">Now we're getting ready to test our app with real users.</p>
       <p className="center">Our partner organizations, MPower Change and CUNY-CLEAR, provide the legal advocacy and community outreach services that really make our platform come to life. You can learn more about them via the links below.</p>
 
-      <div className="col-12 partner-container">
-        <div className="col-6 img-container inline-block">
+      <Grid container direction='row' justify='space-around'>
+        <Grid item xs={4}>
           <a href="http://www.cunyclear.org/" target="_blank" rel="noopener noreferrer">
-            <img src="images/CLEAR.png" />
+            <img src="images/CLEAR.png" style={{maxHeight: '100%', maxWidth: '100%'}}/>
           </a>
-        </div>
+        </Grid>
 
-        <div className="col-6 img-container inline-block">
+        <Grid item xs={4}>
           <a href="https://mpowerchange.org/" target="_blank" rel="noopener noreferrer">
-            <img src="images/mpower.png" />
+            <img src="images/mpower.png" style={{maxHeight: '100%', maxWidth: '100%'}} />
           </a>
-        </div>
-      </div>
+        </Grid>
+      </Grid>
 
-      <div className="col-12">
-        <p className="center">If you'd like to contribute to our project, please check out our Github.</p>
-        <a href="https://github.com/EmilyDev/Border-Buddy" target="_blank" rel="noopener noreferrer">
-          <img className="github-link" src="images/github.png" />
-        </a>
-      </div>
-    </div>
-  </div>
+      <Grid direction='row' container justify="space-around">
+        <Grid item xs={12}>
+          <p className="center">If you'd like to contribute to our project, please check out our Github.</p>
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <a href="https://github.com/EmilyDev/Border-Buddy" target="_blank" rel="noopener noreferrer">
+            <img className="github-link" src="images/github.png" style={{maxHeight: '100%', maxWidth: '100%'}} />
+          </a>
+        </Grid>
+      </Grid>
+    </Grid>
+  </Grid>
 )
--- packages/client/src/utils/muiTheme.tsx
+++ packages/client/src/utils/muiTheme.tsx
@@ -1,48 +1,24 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
-// import { fade } from '@material-ui/core/styles/colorManipulator'
-
-// const darkBlack = common.darkBlack
-// const fullBlack = common.fullBlack
-
 const customTheme = createMuiTheme({
-  // spacing: spacing,
-  // fontFamily: '"Josefin Sans", sans-serif',
+  typography: {
+    fontFamily: '"Josefin Sans", sans-serif',
+  },
   palette: {
     primary: {
       main: '#2d6ea8',
-    }, // light blue
-    // primary2Color: '#2d6ea8', // light blue
-    // primary3Color: '#FFFFFF', // white
-    // accent1Color: '#2d6ea8', // light blue
-    // accent2Color: '#2d6ea8', // light blue
-    // accent3Color: '#2d6ea8', // light blue
-    // textColor: '#000000', // black
-    // secondaryTextColor: '#2d6ea8', // light blue
-    // alternateTextColor: '#FFFFFF', // white
-    // canvasColor: '#FFFFFF', // white
-    // borderColor: '#C0C0C0', // light grey
-    // disabledColor: darkBlack,
-    // pickerHeaderColor: '#2d6ea8',
-    // clockCircleColor: darkBlack,
-    // shadowColor: fullBlack,
+      dark: '#1A4061',
+      light: '#3683C7',
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      main: '#25A8A0', // teal can be made to be a warm color too, but a few sprites also had teal
+      dark: '#176963',
+      light: '#28B5AC',
+      contrastText: '#FFFFFF',
+    },
   },
 })
 
 export default customTheme
 
-// export interface ThemeOptions {
-//   shape?: ShapeOptions;
-//   breakpoints?: BreakpointsOptions;
-//   direction?: Direction;
-//   mixins?: MixinsOptions;
-//   overrides?: Overrides;
-//   palette?: PaletteOptions;
-//   props?: ComponentsProps;
-//   shadows?: Shadows;
-//   spacing?: SpacingOptions;
-//   transitions?: TransitionsOptions;
-//   typography?: TypographyOptions | ((palette: Palette) => TypographyOptions);
-//   zIndex?: ZIndexOptions;
-//   unstable_strictMode?: boolean;
-// }
