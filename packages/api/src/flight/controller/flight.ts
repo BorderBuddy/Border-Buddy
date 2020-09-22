40d4dc6931677d48f24a24e995ab6001a838e75e
Aaron Arntz <aarntz@gmail.com>
2020-09-22 13:29:09 -0400

    add flight stats dev flag env

--- packages/api/src/flight/controller/flight.ts
+++ packages/api/src/flight/controller/flight.ts
@@ -1,5 +1,6 @@
 import prodController from './flight.prod'
 import devController from './flight.dev'
 
+console.log(`FLIGHT_STATS_DEV: ${process.env.FLIGHT_STATS_DEV}`)
 console.log(`flight.dev: ${process.env.npm_config_dev}`)
-export default process.env.npm_config_dev ? devController : prodController
+export default (process.env.npm_config_dev && process.env.FLIGHT_STATS_DEV) ? devController : prodController
